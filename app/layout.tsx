937fa9368808aefdd609527b56250c01fdbbc58a
hyeok <nohyeog8@gmail.com>
2025-11-19 01:43:36 +0900

    chore: ESlint + Prettier 설정(팀 규칙) (#04)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import type { Metadata } from "next";
-import "./globals.css";
-
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "Taskify",
-  description: "Taskify",
+  title: 'Taskify',
+  description: 'Taskify',
 };
 
 export default function RootLayout({
@@ -14,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
