bb1b72e7bb692b74a097418ad1f873b81993491e
hyeok <nohyeog8@gmail.com>
2025-11-19 01:03:48 +0900

    feat: 프로젝트 설치 (#04)

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Taskify",
+  description: "Taskify",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
