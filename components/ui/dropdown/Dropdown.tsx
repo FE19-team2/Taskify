af7ed3aaaac7a96043ecb041b539d08472365cc8
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-11-25 15:18:50 +0900

    feat: Dropdown.tsx생성(#65)

---
+++ components/ui/dropdown/Dropdown.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { FC } from 'react';
+import type { SVGProps } from 'react';
+import { cn } from '@/lib/utils/twmerge'; // twMerge 또는 clsx 사용
+import { Icon, IconName } from '../Icons/Icon';
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface DropdownProps {
+  label: string;
+  isOpen: boolean;
+  options: Option[];
+  onToggle: () => void;
+  onSelect: (value: string) => void;
+  icon?: FC<SVGProps<SVGSVGElement>>;
+}
+
+const Dropdown: FC<DropdownProps> = ({ label, isOpen, options, onToggle, onSelect, icon }) => {
+  return (
+    <div className="relative inline-block ">
+      <button
+        type="button"
+        onClick={onToggle}
+        className={cn(
+          'w-[105px] h-[54px] flex justify-between items-center px-5 py-1.5 rounded-[14px] transition',
+          !isOpen &&
+            'bg-black-400 border border-gray-500 text-gray-200 hover:bg-gray-700 hover:border hover:border-gray-50 hover:text-gray-100',
+          isOpen && 'bg-black-400 text-gray-100 ring-2 ring-gray-500 border border-gray-200',
+        )}
+      >
+        <span>{label}</span>
+        {icon && (
+          <Icon
+            name="ArrowDown"
+            className={cn(
+              'w-[18px] h-[18px] transition-transform duration-200 text-gray-200 hover:text-white ',
+              isOpen ? 'rotate-180' : 'rotate-0',
+            )}
+          />
+        )}
+      </button>
+
+      {isOpen && (
+        <ul
+          className={cn(
+            'absolute w-[260px] h-[252px] overflow-y-auto mt-2 shadow-lg z-50 -left-38',
+            'px-1 py-2.5 rounded-[14px] bg-black-400 border border-gray-500',
+          )}
+        >
+          {options.map((opt) => (
+            <li
+              key={opt.value}
+              onClick={() => onSelect(opt.value)}
+              className="px-4 py-2 hover:bg-gray-700 cursor-pointer text-gray-200 rounded-[10px]"
+            >
+              {opt.label}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
