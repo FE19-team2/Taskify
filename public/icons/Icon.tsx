60b0d55060be1fdf048707a21ffa81c23ff23c5e
DESKTOP-6FVFHUO\기훈 <tjsrlgns97@gmail.com>
2025-11-25 15:32:34 +0900

    refactor: Icon.tsx 생성 (#67)

---
+++ public/icons/Icon.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { IconMap, IconName } from './IconMap';
+
+export type { IconName };
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  name: IconName;
+}
+
+export const Icon: React.FC<IconProps> = ({ name, ...props }) => {
+  const TargetIcon = IconMap[name];
+
+  if (!TargetIcon) {
+    console.warn(`Icon "${name}" not found in IconMap.`);
+    return null;
+  }
+
+  return <TargetIcon {...props} />;
+};
