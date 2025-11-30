d2b3619b38bd55fb6703eb71d83414966c94f3b2
glowhyeon <shyeon710@gmail.com>
2025-11-30 21:08:27 +0900

    feat: 회원가입 페이지 UI 및 폼 컴포넌트 구현

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
-import LogoImage from '@/public/images/logo.svg';
 import Link from 'next/link';
+import LogoImage from '@/public/images/logo.svg';
+import SignupForm from '@/app/signup/_components/SignupForm';
 
 interface SignUpForm {
   email: string;
@@ -34,54 +35,20 @@ interface ErrorResponse {
 
 export default function Page() {
   return (
-    <div className="my-[195px]">
-      <div className="w-[520px] h-[690px] mx-auto">
-        <div className="w-[340px] h-[87px] mb-10 flex items-center justify-center mx-auto mb-8">
+    <div className="my-[195px] flex justify-center items-center">
+      <div className="w-[520px] h-[690px] flex justify-center items-center flex-col gap-10">
+        <div className="mx-auto">
           <Link href="/">
-            <Image
-              src={LogoImage}
-              alt="태스키파이 로고"
-              width={340}
-              height={87}
-              className="w-[98%]"
-            />
+            <Image src={LogoImage} alt="태스키파이 로고" width={340} height={87} />
           </Link>
         </div>
-        <form method="POST">
-          <div className="mb-3">이메일</div>
-          <input
-            type="email"
-            name="email"
-            className="w-full h-[54px] mb-4 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <div className="mb-3">닉네임</div>
-          <input
-            type="text"
-            name="nickname"
-            className="w-full h-[54px] mb-4 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <div className="mb-3">비밀번호</div>
-          <input
-            type="password"
-            name="password"
-            className="w-full h-[54px] mb-4 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <div className="mb-3">비밀번호 확인</div>
-          <input
-            type="password"
-            name="confirmPassword"
-            className="w-full h-[54px] mb-4 px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-          />
-          <div className="mb-10">
-            <input type="checkbox" /> 이용약관에 동의합니다.
-          </div>
-          <button type="submit" className="w-full h-[60px] mb-5 bg-brand-500 cursor-pointer">
-            회원가입
-          </button>
-          <div className="text-center">
-            이미 회원이신가요? <Link href="/login">로그인하기</Link>
-          </div>
-        </form>
+        <SignupForm />
+        <div className="text-center -mt-[30px] text-gray-400 whitespace-pre">
+          이미 회원이신가요?{' '}
+          <Link href="/login" className="text-gray-300">
+            로그인하기
+          </Link>
+        </div>
       </div>
     </div>
   );
